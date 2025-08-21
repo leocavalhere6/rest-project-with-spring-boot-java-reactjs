b0979c2c76b2559ff7a5a0c87acae3dc4ce8290c
leonardo Henrique De Oliveira Cavalhere <leocavalhere6@gmail.com>
2025-08-21 22:09:49 +0000

    feat(file/exporter): add PersonExporter interface for export operations

---
+++ src/main/java/com/leocavalhere/restproject/file/contract/PersonExporter.java
@@ -0,0 +1,14 @@
+package com.leocavalhere.restproject.file.contract;
+
+import com.leocavalhere.restproject.data.dto.PersonDTO;
+import org.springframework.core.io.Resource;
+
+import java.util.List;
+
+public interface PersonExporter {
+
+    Resource exportPeople(List<PersonDTO> people) throws Exception;
+    Resource exportPerson(PersonDTO person) throws Exception;
+}
+
+
