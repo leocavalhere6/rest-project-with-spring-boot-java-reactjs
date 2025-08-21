930fc6812985a1981d8e3547e4bd2ab36c6a3f00
leonardo Henrique De Oliveira Cavalhere <leocavalhere6@gmail.com>
2025-08-21 22:37:50 +0000

    feat(importer/contract): add FileImporter interface

---
+++ src/main/java/com/leocavalhere/restproject/file/importer/factory/FileImporterFactory.java
@@ -0,0 +1,31 @@
+package com.leocavalhere.restproject.file.importer.factory;
+
+import com.leocavalhere.restproject.exception.BadRequestException;
+import com.leocavalhere.restproject.file.importer.contract.FileImporter;
+import com.leocavalhere.restproject.file.importer.impl.CsvImporter;
+import com.leocavalhere.restproject.file.importer.impl.XlsxImporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileImporterFactory {
+
+  private Logger logger = LoggerFactory.getLogger(FileImporterFactory.class);
+
+  @Autowired
+  private ApplicationContext context;
+
+  public FileImporter getImporter(String fileName) throws Exception {
+    if (fileName.endsWith(".xlsx")) {
+      return context.getBean(XlsxImporter.class);
+    } else if (fileName.endsWith(".csv")) {
+      return context.getBean(CsvImporter.class);
+    } else {
+      throw new BadRequestException("Invalid File Format!");
+    }
+  }
+
+}
