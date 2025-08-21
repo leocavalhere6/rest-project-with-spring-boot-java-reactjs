83f5a06de5c700ee43640078941822b645aa6c27
leonardo Henrique De Oliveira Cavalhere <leocavalhere6@gmail.com>
2025-08-21 22:32:19 +0000

    feat(importer/contract): add FileImporter interface

---
+++ src/main/java/com/leocavalhere/restproject/file/importer/contract/FileImporter.java
@@ -0,0 +1,11 @@
+package com.leocavalhere.restproject.file.importer.contract;
+
+import br.com.erudio.data.dto.PersonDTO;
+
+import java.io.InputStream;
+import java.util.List;
+
+public interface FileImporter {
+
+  List<PersonDTO> importFile(InputStream inputStream) throws Exception;
+}
